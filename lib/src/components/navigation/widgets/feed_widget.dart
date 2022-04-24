e2a72633fa230e4c0460ffcfe683f486eca4f046
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2022-04-24 13:55:29 +0100

    Improved structure

--- lib/src/components/navigation/widgets/feed_widget.dart
+++ lib/src/components/navigation/widgets/feed_widget.dart
@@ -53,5 +53,5 @@ class FeedWidget extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(80.h);
+  Size get preferredSize => Size.fromHeight(60.h);
 }
